4609914095d4b838c10cf13a48d6295e52d6ff12
forza111 <nikitos.ionkin@yandex.ru>
2022-01-19 01:00:23 +0300

    add celery task

--- project/routers.py
+++ project/routers.py
@@ -1,10 +1,23 @@
+from celery.result import AsyncResult
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 from schemas import Item
+from worker import create_task
 
 
 app = APIRouter()
 
 @app.post("/tasks")
-def run_task(item: Item):
-    return item
+def run_request(item: Item):
+    task = create_task.delay(item.url)
+    return JSONResponse({"task_id": task.id})
+
+@app.get("/tasks/{task_id}")
+def get_result(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JSONResponse(result)
--- project/worker.py
+++ project/worker.py
@@ -8,3 +8,8 @@ import requests
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+
+@celery.task(name="create_task")
+def create_task(url):
+    response = requests.get(url)
+    return response.status_code
