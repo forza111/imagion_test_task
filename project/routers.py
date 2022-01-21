2bf3edb46f057e96a56b60cdeeab487d1b363eec
forza111 <nikitos.ionkin@yandex.ru>
2022-01-21 11:53:30 +0300

    change worker

--- project/routers.py
+++ project/routers.py
@@ -1,6 +1,7 @@
 from celery.result import AsyncResult
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 
 from schemas import Item
 from worker import create_task
@@ -16,8 +17,8 @@ def run_request(item: Item):
 @app.get("/tasks/{task_id}")
 def get_result(task_id):
     task_result = AsyncResult(task_id)
-    result = {
-        "task_status": task_result.status,
-        "task_result": task_result.result
-    }
-    return JSONResponse(result)
+    if task_result.failed():
+        status_code, msg = (','.join(task_result.result.args)).split(' ',1)
+        raise HTTPException(int(status_code), msg)
+    return task_result.result
+
--- project/worker.py
+++ project/worker.py
@@ -1,7 +1,8 @@
 import os
-import time
 
+from bs4 import BeautifulSoup
 from celery import Celery
+from fastapi import HTTPException
 import requests
 
 
@@ -11,5 +12,20 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://lo
 
 @celery.task(name="create_task")
 def create_task(url):
-    response = requests.get(url)
-    return response.status_code
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except HTTPException as err:
+        raise err
+    else:
+        soup = BeautifulSoup(response.text, 'lxml')
+        result = {}
+        for tag in soup.find_all(True):
+            if tag.find(True):
+                nested = []
+                [nested.append(i.name) for i in tag.find_all(True)]
+                if not result.get(tag.name):
+                    result[tag.name] = {"count": 1, "nested": len(nested)}
+                else:
+                    result.pop(tag.name)
+        return result
