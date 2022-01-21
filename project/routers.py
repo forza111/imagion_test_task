6d40393a905b9d5f5e4f093da0163e7fa5e62137
forza111 <nikitos.ionkin@yandex.ru>
2022-01-21 12:33:02 +0300

    add docstrings

--- project/routers.py
+++ project/routers.py
@@ -11,11 +11,49 @@ app = APIRouter()
 
 @app.post("/tasks")
 def run_request(item: Item):
+    """
+    ## Parsing the page to get unique tags with nested elements count
+
+    Body Parameters
+    ----------
+    param: {"url": "HttpURL"}
+
+    Returns
+    -------
+    str task_id
+    """
     task = create_task.delay(item.url)
     return JSONResponse({"task_id": task.id})
 
 @app.get("/tasks/{task_id}")
 def get_result(task_id):
+    """
+    ## Getting the result of the analysis
+
+    Path Parameters
+    ----------
+    param: str task_id
+
+    Returns
+    -------
+    JSON result.
+    When an error occurs, the status and description are displayed.
+    #### example:
+                {
+                  "html": {
+                    "count": 1,
+                    "nested": 633
+                  },
+                  "head": {
+                    "count": 1,
+                    "nested": 40
+                  },
+                  "body": {
+                    "count": 1,
+                    "nested": 591
+                  }
+               }
+    """
     task_result = AsyncResult(task_id)
     if task_result.failed():
         status_code, msg = (','.join(task_result.result.args)).split(' ',1)
--- project/worker.py
+++ project/worker.py
@@ -12,6 +12,31 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://lo
 
 @celery.task(name="create_task")
 def create_task(url):
+    """
+    Execution of a get request based on the given URL.
+    On a successful request, the page is parsed using the bs4 library
+
+    :param url: HttpURL
+    :return:
+    dict: The number of unique tags with the number of nested elements.
+    example:
+            {
+              "html": {
+                "count": 1,
+                "nested": 633
+              },
+              "head": {
+                "count": 1,
+                "nested": 40
+              },
+              "body": {
+                "count": 1,
+                "nested": 591
+              }
+           }
+    All duplicate tags are removed.
+    If a request error occurs, an HttpError is returned.
+    """
     try:
         response = requests.get(url)
         response.raise_for_status()
